e8e680bc7741ff47a83fdc7ea62cf2cbac292843
Gariel Abeno <gabrielabeno@Microsoft.com>
2022-10-27 12:31:53 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import {Routes, Route} from "react-router-dom"
+import {Container} from "react-bootstrap"
+
+function Home() {
+  return <h1>Home Page</h1>
+}
+
+function Store() {
+  return <h1>Store Page</h1>
+}
+
+function About() {
+  return <h1>About Page</h1>
+}
+
 function App() {
   return (
-    <h1>Hi</h1>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
   )
 }
 
