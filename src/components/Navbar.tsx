1bacc2eafaca82344b620b2e990c7ea292619dc9
Gariel Abeno <gabrielabeno@Microsoft.com>
2022-10-27 12:55:31 +0300

    use Navbar component of bootstrap

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,9 @@
+import {Container, Navbar as NavbarBs } from "react-bootstrap"
+
 export function Navbar() {
-    return <h3>Navbar</h3>
+    return (
+        <NavbarBs className="bg-white shadow-sm mb-3">
+            <Container>Nav</Container>
+        </NavbarBs>
+    )
 }
