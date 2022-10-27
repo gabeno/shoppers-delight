0249a8f08818137cefaf91a158a46a3208c24946
Gariel Abeno <gabrielabeno@Microsoft.com>
2022-10-27 13:05:51 +0300

    add links to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
-import {Container, Navbar as NavbarBs } from "react-bootstrap"
+import {Container, Nav, Navbar as NavbarBs } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
 
 export function Navbar() {
     return (
         <NavbarBs className="bg-white shadow-sm mb-3">
-            <Container>Nav</Container>
+            <Container>
+                <Nav>
+                    <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                    <Nav.Link as={NavLink} to="/store">Store</Nav.Link>
+                    <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+                </Nav>
+            </Container>
         </NavbarBs>
     )
 }
