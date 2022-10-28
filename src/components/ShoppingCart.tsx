70e79b27d981b98e832b0b01e0a70a24fe9bb8bb
Gariel Abeno <gabrielabeno@Microsoft.com>
2022-10-28 21:31:22 +0300

    add shopping cart total

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utilities/formatCurrency";
 import { CartItem } from "./CartItem";
+import storeItems from "../data/items.json"
 
 type ShoppinCartProps = {
     isOpen: boolean
@@ -19,6 +21,14 @@ export function ShoppingCart({isOpen}: ShoppinCartProps) {
                     {cartItems.map(item => (
                         <CartItem key={item.id} {...item} />
                     ))}
+                    <div className="ms-auto fs-5">
+                        Total {formatCurrency(
+                            cartItems.reduce((total, cartItem) => {
+                                const item = storeItems.find(i => i.id === cartItem.id)
+                                return total + (item?.price || 0) * cartItem.quantity
+                            }, 0)
+                        )}
+                    </div>
                 </Stack>
             </Offcanvas.Body>
         </Offcanvas>
