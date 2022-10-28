feeff83bf0b446f0176b417cbf19f349bbfc2721
Gariel Abeno <gabrielabeno@Microsoft.com>
2022-10-28 18:10:09 +0300

    add CartItem component to show items in cart

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,18 +1,26 @@
-import { Offcanvas } from "react-bootstrap";
+import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { CartItem } from "./CartItem";
 
 type ShoppinCartProps = {
     isOpen: boolean
 }
 
 export function ShoppingCart({isOpen}: ShoppinCartProps) {
-    const {closeCart} = useShoppingCart()
+    const {closeCart, cartItems} = useShoppingCart()
 
     return (
         <Offcanvas show={isOpen} placement="end" onHide={closeCart}>
             <Offcanvas.Header closeButton>
                 <Offcanvas.Title>Cart</Offcanvas.Title>
             </Offcanvas.Header>
+            <Offcanvas.Body>
+                <Stack gap={3}>
+                    {cartItems.map(item => (
+                        <CartItem key={item.id} {...item} />
+                    ))}
+                </Stack>
+            </Offcanvas.Body>
         </Offcanvas>
     )
 }
